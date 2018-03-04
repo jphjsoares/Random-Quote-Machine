77a2a6debfae8e391af4b3264ffb07a286219ea0
José Soares <josephjsoares@aecastrodaire.com>
2018-03-04 13:58:40 +0000

    Refactor and TODO's update
    Refactoring API logic file.
    Adding future TODO's.

--- _javascript/APILogic.js
+++ _javascript/APILogic.js
@@ -1,22 +1,45 @@
 $(document).ready(function(){
 
-      function randomQuote() {
-        $.ajax({
-          url: "https://api.forismatic.com/api/1.0/?",
-          dataType: "jsonp",
-          data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
-          success: function( response ) {
-            $("#quote-content").html("<p id='quote-content'>" + response.quoteText + "</p><br/><p id='quote-author'>&dash;" + response.quoteAuthor + "</p>");
-            $("#tweet-this").attr("href", "https://twitter.com/home/?status=" + response.quoteText + ' (' + response.quoteAuthor + ')');
-          }
-        });
-      }
-      $(function() {
-        randomQuote();
-      });
-      $("#new-quote").click(function(){
-        randomQuote();
-      });
+	      function randomQuote() {
+
+	        $.ajax({
+	          url: "https://api.forismatic.com/api/1.0/?",
+	          dataType: "jsonp",
+	          data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
+	          success: function( response ) {
+
+	            var content = response.quoteText;
+	            var author = response.quoteAuthor;
+
+	            //Put the content and author into the page
+	            $("#quote-content").html("<p id='quote-content'>" + content + "</p><br/><p id='quote-author'>&dash;" + author + "</p>");
+
+	            //Model the tweet
+	            $("#tweet-this").attr("href", "https://twitter.com/home/?status=" + content + ' -' + author);
+
+	          }
+
+	        });
+
+	      }
+
+	      $(function() {
+
+	        randomQuote();
+
+	      });
+
+	      $("#new-quote").click(function(){
+
+	        randomQuote();
+
+	      });
 
 
-    });//End of document function
+	    });//End of document function
+
+	/*
+	TODO:
+	  If a quote has the ';' char, replace with ','
+	  Add backend
+	*/
